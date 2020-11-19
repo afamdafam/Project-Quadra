7c9a4e0e8c25b72a5f68b82ebafd99a0bcd52896
mnaufaldillah <mnaufaldillah@gmail.com>
2020-11-19 21:25:11 +0700

    Wrapped Candies, Generating Bombs 2, and Bug Fixes
    Part 22, Part 23, Part 24
    
    Known problem: klik kanan mouse membuat adjacent bomb(wrapped candies)(untuk testing).

--- Assets/Scripts/FindMatches.cs
+++ Assets/Scripts/FindMatches.cs
@@ -18,6 +18,26 @@ public class FindMatches : MonoBehaviour
         StartCoroutine(FindAllMatchesCo());
     }
 
+    private List<GameObject> IsAdjacentBomb(Dot dot1, Dot dot2, Dot dot3)
+    {
+        List<GameObject> currentDots = new List<GameObject>();
+        if (dot1.isAdjacentBomb)
+        {
+            currentMatches.Union(GetAdjacentPieces(dot1.column, dot1.row));
+        }
+
+        if (dot2.isAdjacentBomb)
+        {
+            currentMatches.Union(GetAdjacentPieces(dot2.column, dot2.row));
+        }
+
+        if (dot3.isAdjacentBomb)
+        {
+            currentMatches.Union(GetAdjacentPieces(dot3.column, dot3.row));
+        }
+        return currentDots;
+    }
+
     private List<GameObject> IsRowBomb(Dot dot1, Dot dot2, Dot dot3)
     {
         List<GameObject> currentDots = new List<GameObject>();
@@ -97,6 +117,8 @@ public class FindMatches : MonoBehaviour
 
                                     currentMatches.Union(IsColumnBomb(leftDotDot, currentDotDot, rightDotDot));
 
+                                    currentMatches.Union(IsAdjacentBomb(leftDotDot, currentDotDot, rightDotDot));
+
                                     GetNearbyPieces(leftDot, currentDot, rightDot);
 
                                 }
@@ -119,6 +141,8 @@ public class FindMatches : MonoBehaviour
                                     currentMatches.Union(IsColumnBomb(upDotDot, currentDotDot, downDotDot));
 
                                     currentMatches.Union(IsRowBomb(upDotDot, currentDotDot, downDotDot));
+
+                                    currentMatches.Union(IsAdjacentBomb(upDotDot, currentDotDot, downDotDot));
 
                                     GetNearbyPieces(upDot, currentDot, downDot);
                                 }
@@ -149,6 +173,24 @@ public class FindMatches : MonoBehaviour
                 }
             }
         }
+    }
+
+    List<GameObject> GetAdjacentPieces(int column, int row)
+    {
+        List<GameObject> dots = new List<GameObject>();
+        for(int i = column - 1; i <= column + 1; i++)
+        {
+            for(int j = row - 1; j <= row + 1; j++)
+            {
+                //Check if the piece is inside the board
+                if(i >= 0 && i < board.width && j >= 0 && j < board.height)
+                {
+                    dots.Add(board.allDots[i, j]);
+                    board.allDots[i, j].GetComponent<Dot>().isMatched = true;
+                }
+            }
+        }
+        return dots;
     }
 
     List<GameObject> GetColumnPieces(int column)
